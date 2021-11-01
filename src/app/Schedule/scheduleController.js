acf0e8761a3c234574667f56c6ffdd25a189d290
yunju <tpfffl0420@naver.com>
2021-11-01 18:22:42 +0900

    commit change

--- src/app/Schedule/scheduleController.js
+++ src/app/Schedule/scheduleController.js
@@ -6,11 +6,29 @@ const {response, errResponse} = require("../../../config/response");
 
 const regexEmail = require("regex-email");
 const {emit} = require("nodemon");
+const userProvider = require("../../app/User/userProvider");
+
 
 /**
- * API No. 0
- * API Name : 테스트 API
- * [GET] /app/useIds/:userId/schedules
- *
+ * API No. 5
+ * API Name : 나의 스케줄 추가
+ * [POST] /app/useIds/:userId/schedules
  */
+exports.postSchedule = async function (req, res) {
+
+    /**
+     * body : userId, activatedTime, activatedDay, openTo(1-팀원, 2 나만), nameOpenTo,
+     *
+     */
+    const {userId, activatedTime, activatedDay, openTo, nameOpenTo} = req.body;
+
+    if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
+    if (!activatedTime) return res.send(errResponse(baseResponse.SCHEDULE_ACTIVATEDTIME_EMPTY));
+    if (!activatedDay) return res.send(errResponse(baseResponse.SCHEDULE_ACTIVATEDAY_EMPTY));
+
+    const scheduleAdd = await scheduleService.retrieveSchedulePost(
+        {userId, activatedTime, activatedDay, openTo, nameOpenTo}
+    );
+    return res.send(response(baseResponse.SUCCESS, scheduleAdd));
+};
 
--- src/app/Schedule/scheduleService.js
+++ src/app/Schedule/scheduleService.js
@@ -10,15 +10,43 @@ const {errResponse} = require("../../../config/response");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const {connect} = require("http2");
+const userProvider = require("./userProvider");
+const userDao = require("./userDao");
 
 // Service: Create, Update, Delete 비즈니스 로직 처리
-exports.postSchedule = async function () {
+exports.retrieveSchedulePost = async function (userId, activatedTime, activatedDay, openTo, nameOpenTo) {
     try{
+        //이미 존재하는 스케줄이 있는지 학인하기
+        const scheduleRows = await userProvider.scheduleCheck(userId, activatedTime);
+
+        //있다면, 에러
+
+        //없다면, 진행
+
+    }
+    catch (err){
 
 
     }
-    catch (err){}
 
+    // 이메일 중복 확인
+    const emailRows = await userProvider.emailCheck(email);
+    if (emailRows.length > 0)
+        return errResponse(baseResponse.SIGNUP_REDUNDANT_EMAIL);
 
+    // 비밀번호 암호화
+    const hashedPassword = await crypto
+        .createHash("sha512")
+        .update(password)
+        .digest("hex");
+
+    const insertUserInfoParams = [email, hashedPassword, nickname];
+
+    const connection = await pool.getConnection(async (conn) => conn);
+
+    const userIdResult = await userDao.insertUserInfo(connection, insertUserInfoParams);
+    console.log(`추가된 회원 : ${userIdResult[0].insertId}`)
+    connection.release();
+    return response(baseResponse.SUCCESS);
 
 }
